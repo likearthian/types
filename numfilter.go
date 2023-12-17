7a7ae42a26e2b40292387ee0ee89eaf57b7acce1
Ziska Zarkasyi <ziska.zarkasyi@gmail.com>
2023-12-17 12:14:01 +0700

    Add generic NumFilter type and methods
    This commit adds a new generic NumFilter type and its corresponding methods for unmarshaling and marshaling text. The NumFilter type can handle both int64 and float64 values. This change improves the flexibility and reusability of the code.

--- numfilter.go
+++ numfilter.go
@@ -6,10 +6,51 @@ import (
 	"strings"
 )
 
+type NumFilter[T int64 | float64] struct {
+	Valid bool
+	Comp  string
+	Value T
+}
+
+func (nf *NumFilter[T]) UnmarshalText(text []byte) error {
+	if len(text) == 0 {
+		*nf = NumFilter[T]{
+			Valid: false,
+		}
+		return nil
+	}
+
+	valid, comp, val, err := parseGenericNumFilter[T](string(text))
+	if err != nil {
+		return err
+	}
+
+	*nf = NumFilter[T]{
+		Valid: valid,
+		Comp:  comp,
+		Value: val,
+	}
+
+	return nil
+}
+
+func (nf *NumFilter[T]) MarshalText() (text []byte, err error) {
+	if !nf.Valid {
+		return []byte{}, nil
+	}
+
+	bld := strings.Builder{}
+	bld.WriteString(fmt.Sprintf("%v", nf.Value))
+	bld.WriteString("|")
+	bld.WriteString(nf.Comp)
+
+	return []byte(bld.String()), nil
+}
+
 type IntFilter struct {
 	Valid bool
-	comp  string
-	value int64
+	Comp  string
+	Value int64
 }
 
 func (i *IntFilter) UnmarshalText(text []byte) error {
@@ -27,8 +68,8 @@ func (i *IntFilter) UnmarshalText(text []byte) error {
 
 	*i = IntFilter{
 		Valid: valid,
-		comp:  comp,
-		value: int64(val),
+		Comp:  comp,
+		Value: int64(val),
 	}
 
 	return nil
@@ -40,17 +81,17 @@ func (i *IntFilter) MarshalText() (text []byte, err error) {
 	}
 
 	bld := strings.Builder{}
-	bld.WriteString(strconv.FormatInt(i.value, 10))
+	bld.WriteString(strconv.FormatInt(i.Value, 10))
 	bld.WriteString("|")
-	bld.WriteString(i.comp)
+	bld.WriteString(i.Comp)
 
 	return []byte(bld.String()), nil
 }
 
 type FloatFilter struct {
 	Valid bool
-	comp  string
-	value float64
+	Comp  string
+	Value float64
 }
 
 func (f *FloatFilter) UnmarshalText(text []byte) error {
@@ -68,8 +109,8 @@ func (f *FloatFilter) UnmarshalText(text []byte) error {
 
 	*f = FloatFilter{
 		Valid: valid,
-		comp:  comp,
-		value: val,
+		Comp:  comp,
+		Value: val,
 	}
 
 	return nil
@@ -81,9 +122,9 @@ func (f *FloatFilter) MarshalText() (text []byte, err error) {
 	}
 
 	bld := strings.Builder{}
-	bld.WriteString(strconv.FormatFloat(f.value, 'f', -1, 64))
+	bld.WriteString(strconv.FormatFloat(f.Value, 'f', -1, 64))
 	bld.WriteString("|")
-	bld.WriteString(f.comp)
+	bld.WriteString(f.Comp)
 
 	return []byte(bld.String()), nil
 }
@@ -107,6 +148,32 @@ func parseNumFilter(s string) (valid bool, comp string, val float64, err error) 
 	if err != nil {
 		return false, "", 0, err
 	}
+
+	valid = true
+	return
+}
+
+func parseGenericNumFilter[T int64 | float64](s string) (valid bool, comp string, val T, err error) {
+	strs := strings.Split(string(s), "|")
+	valstr := strings.TrimSpace(strs[0])
+	comp = "eq"
+	if len(strs) > 1 {
+		comp = strings.TrimSpace(strs[1])
+	}
+
+	switch comp {
+	case "eq", "gt", "gte", "lt", "lte", "n", "nn":
+		break
+	default:
+		return false, "", 0, fmt.Errorf("unknown comparison operator %q", comp)
+	}
+
+	fval, err := strconv.ParseFloat(valstr, 64)
+	if err != nil {
+		return false, "", 0, err
+	}
+
+	val = T(fval)
 
 	valid = true
 	return
